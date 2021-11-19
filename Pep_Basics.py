8b467ffcee17c72d1b85aedf3ac76ca76d517ffb
Gowtham Potureddi <gowthampotureddi@Gowthams-MacBook-Pro.local>
2021-11-19 06:25:30 +0530

    Adding basics cde

--- Pep_Basics.py
+++ Pep_Basics.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 class Basics():
     def Z(self,end):
         ans = ''
@@ -10,7 +12,19 @@ class Basics():
             ans = ans+"\n"
         print(ans)
 
+    def isPrime(self,number):
+        if number <= 0:
+            return 
+
+        for i in range(2,int(sqrt(number)+1)):
+            if number % i == 0:
+                return
+        print(str(number) + " Prime ")
 
 if __name__ == '__main__':
     obj = Basics()
-    obj.Z(5)
+    # obj.Z(5)
+    # for i in range(0,100):
+    #     obj.isPrime(i)
+
+    
