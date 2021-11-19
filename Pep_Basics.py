f7995588451954d9134299f53b816d0131f1fbb6
Gowtham Potureddi <gowthampotureddi@Gowthams-MacBook-Pro.local>
2021-11-19 06:40:23 +0530

    Adding basics cde

--- Pep_Basics.py
+++ Pep_Basics.py
@@ -21,10 +21,23 @@ class Basics():
                 return
         print(str(number) + " Prime ")
 
+    def febonacci(self, n):
+        # Recurssion
+        if number < 0: return 
+        if number <= 1: return number
+        return self.febonacci(number-1)+self.febonacci(number-2)
+        # Iteration
+        # f = [0, 1]
+        # for i in range(2, n+1):
+        #     f.append(f[i-1] + f[i-2])
+        # return f[n]
+
+
 if __name__ == '__main__':
     obj = Basics()
     # obj.Z(5)
     # for i in range(0,100):
     #     obj.isPrime(i)
+    # print(obj.febonacci(9))
 
     
