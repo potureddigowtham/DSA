95c77741add236418727bc67fc31f96aafe4f195
Gowtham Potureddi <gowthampotureddi@Gowthams-MacBook-Pro.local>
2021-12-03 10:02:12 +0530

    added array problems

--- pep_strings_level_1.py
+++ pep_strings_level_1.py
@@ -35,15 +35,34 @@ class Solution:
                     counter = 1
                 if i != n-1:
                     ans += s[i]
-            
         print(ans)
 
-    def main(self):
+    def toggle_char(self, s):
+        list1 = 'abcdefghijklmnopqrstuvwxyz'
+        list2 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        ans = ''
+        for i in s:
+            if i in list1:
+                temp1 = list1.find(i)
+                ans += (list2[temp1])
+            elif i in list2:
+                temp2 = list2.find(i)
+                ans += (list1[temp2])
+        print(ans)
+
+    def fac(self, n):
+        if n < 2:
+            return n
+        return self.fac(n-1)+self.fac(n-2)
+
+    # def main(self):
         # self.substrings("abccbc")
         # print(self.palindrome("abaaba"))
         # self.string_compression('aaabbccaaaaaaggggdee')
-
+        # self.toggle_char('GowTHam')
+        # self.print_all_permutations("abc")
+        
 
 if __name__ == "__main__":
     obj = Solution()
-    obj.main()
+    # obj.main()
