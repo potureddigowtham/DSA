0612093398f1e310987488b84a27412f7c828c5d
Gowtham Potureddi <gowthampotureddi@Gowthams-MacBook-Pro.local>
2021-12-20 16:57:22 +0530

    dp

--- level 1/Recursion and Backtracking/pep_recursion_in_array.py
+++ level 1/Recursion and Backtracking/pep_recursion_in_array.py
@@ -18,13 +18,23 @@ class Solution:
         maxi = max(arr[id], self.max_of_array(arr, id+1))
         return maxi
 
-    def first_index(self, arr, id, n):
+    def first_index(self, arr, id, n):  # ignore
         if id > len(arr)-1:
-            return
+            return -1
         self.first_index(arr, id+1, n)
         if arr[id] == n:
             print(id)
             return id
+
+    def first_occurance_1(self, n, x, index, ans):
+        if len(n) <= 0:
+            ans = -1
+            return ans
+        if x == n[0]:
+            ans = index
+            return ans
+        ans = self.first_occurance_1(n[1:], x, index+1, ans)
+        return ans
 
     def last_index(self, arr, id, n):
         if id > len(arr)-1:
@@ -51,9 +61,10 @@ class Solution:
         # self.display_array([10, 20, 30, 40, 50], 0)
         # self.display_array_reverse([10, 20, 30, 40, 50], 0)
         # print(self.max_of_array([1000, 60, 30, 8000, 500], 0))
-        # self.first_index([8000, 500], 0, 500)
+        self.first_index([15, 11, 40, 4, 4, 9], 0, 4)
+        print(self.first_occurance_1([140, 4, 4, 10], 14, 0, 0))
         # print(self.last_index([40, 20, 30], 0, 10))
-        print(self.all_index([10, 10, 10], 0, 10, []))
+        # print(self.all_index([10, 10, 10], 0, 10, []))
 
 
 if __name__ == "__main__":
