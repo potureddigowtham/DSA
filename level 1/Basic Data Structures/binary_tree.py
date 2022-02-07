f407a6a62f7df0ce2536ee82c705d98e4ecb8d7a
Gowtham Potureddi <gowthampotureddi@Gowthams-MacBook-Pro.local>
2022-02-07 09:19:09 +0530

    tress

--- level 1/Basic Data Structures/binary_tree.py
+++ level 1/Basic Data Structures/binary_tree.py
@@ -102,10 +102,48 @@ class binarytree:
         self.inorder_btree(root.left)
         print(root.data)
         self.inorder_btree(root.right)
+
+    def levelorder_btree(self, root):
+        if not root:
+            return []
+        ans, level = [], [root]
+        while level:
+            ans.append([node.data for node in level])
+            temp = []
+            for node in level:
+                temp.extend([node.left, node.right])
+            level = [leaf for leaf in temp if leaf]
+        return ans
+
+    def left_cloned_tree(self, root):
+        if not root:
+            return None
+        lnr = self.left_cloned_tree(root.left)
+        rnr = self.left_cloned_tree(root.right)
+        clone = binarytree(root.data, lnr, None)
+        root.left = clone
+        root.right = rnr
         
+        return root
+
+    def remove_leaf_nodes(self, root):
+        if not root:
+            return None
+
+        if root.left == None and root.right == None:
+            return None
+        
+        root.left = self.remove_leaf_nodes(root.left)
+        root.right = self.remove_leaf_nodes(root.right)
+
+        return root
+
+
+
 
 obj = binarytree()
 tree = obj.btree_list([50,25,12,None,None,37,30,None,None,None,75,62,None,70,None,None,87,None])
+# tree = obj.btree_list([3,9,None,None,20,15,None,None,7,None,None])
 # obj.display_btree(tree) 
 # print(obj.size_btree(tree))
 # print(obj.sum_btree(tree))
@@ -114,4 +152,9 @@ tree = obj.btree_list([50,25,12,None,None,37,30,None,None,None,75,62,None,70,Non
 # print(obj.max_btree(tree))
 # obj.preorder_btree(tree)
 # obj.postorder_btree(tree)
-obj.inorder_btree(tree)
+# obj.inorder_btree(tree)
+# print(obj.levelorder_btree(tree))
+# test = obj.left_cloned_tree(tree)
+# obj.display_btree(test)
+# temp = obj.remove_leaf_nodes(tree)
+# obj.display_btree(temp)
